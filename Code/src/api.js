020f8fee134448c6297adddd3f9c7ef3ae551c24
Kosmic <80226004+KosmicAnomaly@users.noreply.github.com>
2022-11-13 14:48:44 -0700

    Add Google Maps API functions

--- Code/src/api.js
+++ Code/src/api.js
@@ -1,4 +1,7 @@
 const axios = require("axios");
+const { Client } = require("@googlemaps/google-maps-services-js");
+
+const googleMapsClient = new Client({});
 
 const getWeatherGrid = async (latitude, longitude) => {
   let url =
@@ -35,4 +38,40 @@ const getWeatherData = async (latitude, longitude) => {
   };
 };
 
+// origins: [str]
+// destinations: [str]
+// departureTime: Date
+
+// https://googlemaps.github.io/google-maps-services-js/classes/Client.html#distancematrix
+const getRouteDistanceTime = async (origins, destinations, departureTime) => {
+  return googleMapsClient.distancematrix({
+    params: {
+      departure_time: departureTime,
+      destinations: destinations,
+      origins: origins,
+      key: process.env.GOOGLE_MAPS_API_KEY,
+    },
+    timeout: 2000, // Request timeout (in milliseconds)
+  });
+};
+
+// Note: The API Key will need to be restricted to our domain when we push to production, because it will be publicly visible.
+const getGoogleMapEmbed = function (width, height, origin, destination) {
+  return (
+    "<iframe width='" +
+    width +
+    "' height='" +
+    height +
+    "' style='border:0' loading='lazy' allowfullscreen referrerpolicy='no-referrer-when-downgrade' src='https://www.google.com/maps/embed/v1/directions?origin=" +
+    encodeURIComponent(origin) +
+    "&destination=" +
+    encodeURIComponent(destination) +
+    "&key=" +
+    process.env.GOOGLE_MAPS_API_KEY +
+    "'></iframe>"
+  );
+};
+
 module.exports.getWeatherData = getWeatherData;
+module.exports.getRouteDistanceTime = getRouteDistanceTime;
+module.exports.getGoogleMapEmbed = getGoogleMapEmbed;
