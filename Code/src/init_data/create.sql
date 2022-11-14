541bb4eaf6b249017e61e65bde54a80cb42d8c16
koralvillalobos <koral.villalobos@colorado.edu>
2022-11-14 08:32:07 -0700

    cart changes

--- Code/src/init_data/create.sql
+++ Code/src/init_data/create.sql
@@ -29,3 +29,27 @@ CREATE TABLE "trails" (
 CREATE INDEX "idx_trails__ski_mountain" ON "trails" ("ski_mountain");
 
 ALTER TABLE "trails" ADD CONSTRAINT "fk_trails__ski_mountain" FOREIGN KEY ("ski_mountain") REFERENCES "ski_mountains" ("id") ON DELETE CASCADE;
+
+CREATE TABLE "cart" (
+  "id" SERIAL PRIMARY KEY,
+  "user" INTEGER NOT NULL
+);
+
+CREATE INDEX "idx_cart__user" ON "cart" ("user");
+
+ALTER TABLE "cart" ADD CONSTRAINT "fk_cart__user" FOREIGN KEY ("user") REFERENCES "users" ("id");
+
+CREATE TABLE "cartitem" (
+  "id" SERIAL PRIMARY KEY,
+  "cart" INTEGER NOT NULL,
+  "ski_mountain" INTEGER,
+  "hotel" TEXT
+);
+
+CREATE INDEX "idx_cartitem__cart" ON "cartitem" ("cart");
+
+CREATE INDEX "idx_cartitem__ski_mountain" ON "cartitem" ("ski_mountain");
+
+ALTER TABLE "cartitem" ADD CONSTRAINT "fk_cartitem__cart" FOREIGN KEY ("cart") REFERENCES "cart" ("id") ON DELETE CASCADE;
+
+ALTER TABLE "cartitem" ADD CONSTRAINT "fk_cartitem__ski_mountain" FOREIGN KEY ("ski_mountain") REFERENCES "ski_mountains" ("id") ON DELETE SET NULL
